7aec96672c7e3b1b67fb0e36be1133c187a1e150
Eric Klaesson <ericklaesson@gmail.com>
2020-04-12 13:07:12 +0800

    try 1: prod configs setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { AboutComponent } from './about/about.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    AboutComponent
   ],
   imports: [
+    MatToolbarModule,
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      { path: 'welcome', 
+              component: WelcomeComponent 
+      },
+      { path: 'about', 
+              component: AboutComponent 
+      },     
+      { path: '', 
+              redirectTo: 'Welcome', 
+              pathMatch: 'full'
+      },
+      {path: '**', 
+             redirectTo: 'Welcome', 
+             pathMatch: 'full' 
+      },
+    ]),
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
