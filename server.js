dc3768252f163642bce12035087245d47cfa1218
Eric Klaesson <ericklaesson@gmail.com>
2020-04-12 13:42:07 +0800

    try 2: prod configs setup

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/src/'));
+app.use(express.static(__dirname + '/dist/bsb'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/src/index.html'));
+res.sendFile(path.join(__dirname+'/dist/bsb/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, () => {
+    console.log('Server started!')
+});
